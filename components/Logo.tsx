9fb10105a64061a68a3b0c25b1ba828ad7e9881c
haterb4 <haterb2803@gmail.com>
2023-11-17 17:45:40 +0100

    adding posstinstall

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <Link href={'/'} className='font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer'>
-        Geometria Euclidea
+        2D Drawer
     </Link>
   )
 }
